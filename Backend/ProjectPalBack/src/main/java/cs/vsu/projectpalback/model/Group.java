e9171be2d992f46dcadc93d9daf875f22343545a
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-08 21:46:40 +0300

    Created model for group

---
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/model/Group.java
@@ -0,0 +1,22 @@
+package cs.vsu.projectpalback.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "\"group\"")
+@Getter
+@Setter
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "group_number", nullable = false)
+    private int groupNumber;
+
+    @Column(name = "course_number", nullable = false)
+    private int courseNumber;
+}
