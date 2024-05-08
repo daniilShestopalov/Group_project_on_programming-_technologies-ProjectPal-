f8c7b607b08639dd9fafe571097a8cba5e0409e1
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-08 22:41:17 +0300

    Created model for project

---
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/model/Project.java
@@ -0,0 +1,40 @@
+package cs.vsu.projectpalback.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project")
+@Getter
+@Setter
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User teacher;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "file_link", columnDefinition = "TEXT")
+    private String fileLink;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+}
