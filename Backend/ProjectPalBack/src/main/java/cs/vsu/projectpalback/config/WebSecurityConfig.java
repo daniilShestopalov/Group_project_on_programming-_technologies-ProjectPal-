96ee706b458e1dee0e5acd9cc46e22798089e210
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-11 20:32:18 +0300

    Obsolete annotation removed

--- Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/config/WebSecurityConfig.java
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import cs.vsu.projectpalback.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig{
 
