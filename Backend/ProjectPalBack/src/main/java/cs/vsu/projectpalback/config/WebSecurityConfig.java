b18b916424a3abf721cd3b22cc93199cd8974e78
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-11 19:46:49 +0300

    Clean code

--- Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/config/WebSecurityConfig.java
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import cs.vsu.projectpalback.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
