acd6d662ceec1bcf04633634f6aad5cf453f8033
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-12 16:58:26 +0300

    Create TaskAnswerDTO and fixed ProjectAnswerDTO

--- Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/dto/TaskAnswerDTO.java
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/dto/TaskAnswerDTO.java
@@ -1,17 +1,21 @@
 package cs.vsu.projectpalback.dto;
 
+import cs.vsu.projectpalback.model.User;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-public class ProjectAnswerDTO {
+public class TaskAnswerDTO {
 
     private int id;
 
-    @NotNull(message = "Project id cannot be null")
-    private int projectId;
+    @NotNull(message = "Task id cannot be null")
+    private int taskId;
+
+    @NotNull(message = "Student user id cannot be null")
+    private User student;
 
     @NotNull(message = "Submission date cannot be null")
     private LocalDateTime submissionDate;
