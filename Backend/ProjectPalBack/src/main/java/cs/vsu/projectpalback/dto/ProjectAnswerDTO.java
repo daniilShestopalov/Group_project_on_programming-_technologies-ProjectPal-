ad3c22d0a7ffb0445e5686c03b296313c162eb17
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-12 16:52:09 +0300

    Create ProjectAnswerDTO

---
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/dto/ProjectAnswerDTO.java
@@ -0,0 +1,27 @@
+package cs.vsu.projectpalback.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ProjectAnswerDTO {
+
+    private int id;
+
+    @NotNull(message = "Project name cannot be null")
+    private int projectId;
+
+    @NotNull(message = "Project name cannot be null")
+    private LocalDateTime submissionDate;
+
+    private String teacherCommentary;
+
+    private String studentCommentary;
+
+    private Integer grade;
+
+    @NotNull(message = "File link cannot be null")
+    private String fileLink;
+}
