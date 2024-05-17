a3099dfd0188c6ddfa8edf0aa4b580a0feea62d2
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-17 19:56:58 +0300

    Created TaskService

--- Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/mapper/TaskMapper.java
+++ Backend/ProjectPalBack/src/main/java/cs/vsu/projectpalback/mapper/TaskMapper.java
@@ -8,6 +8,7 @@ import cs.vsu.projectpalback.repository.GroupRepository;
 import cs.vsu.projectpalback.repository.UserRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -33,6 +34,10 @@ public abstract class TaskMapper {
     public abstract List<TaskDTO> toDtoList(List<Task> tasks);
 
     public abstract List<Task> toEntityList(List<TaskDTO> taskDTOs);
+
+    @Mapping(source = "teacherUserId", target = "teacher", qualifiedByName = "userFromId")
+    @Mapping(source = "groupId", target = "group", qualifiedByName = "groupFromId")
+    public abstract void updateEntityFromDto(TaskDTO dto, @MappingTarget Task entity);
 
     @Named("userFromId")
     protected User userFromId(int id) {
