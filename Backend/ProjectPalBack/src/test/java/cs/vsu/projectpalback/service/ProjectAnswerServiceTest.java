d8a284bb48159d938132626675c6824d4d8c80dc
ImMortalUser <ivan.grigorij@yandex.ru>
2024-05-22 15:13:02 +0300

    Added tests, improved docker and gradle files

---
+++ Backend/ProjectPalBack/src/test/java/cs/vsu/projectpalback/service/ProjectAnswerServiceTest.java
@@ -0,0 +1,109 @@
+package cs.vsu.projectpalback.service;
+
+import cs.vsu.projectpalback.dto.ProjectAnswerDTO;
+import cs.vsu.projectpalback.dto.ProjectDTO;
+import cs.vsu.projectpalback.dto.UserDTO;
+import cs.vsu.projectpalback.model.enumerate.Role;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@Rollback(true)
+public class ProjectAnswerServiceTest {
+
+    @Autowired
+    private ProjectAnswerService projectAnswerService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Test
+    public void testGetAllProjectAnswers() {
+        List<ProjectAnswerDTO> result = projectAnswerService.getAllProjectAnswers();
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testGetProjectAnswerById() {
+        Integer id = 1;
+        ProjectAnswerDTO result = projectAnswerService.getProjectAnswerById(id);
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetProjectAnswerByProjectId() {
+        Integer projectId = 1;
+        ProjectAnswerDTO result = projectAnswerService.getProjectAnswerByProjectId(projectId);
+        assertNull(result);
+    }
+
+    @Test
+    public void testCreateProjectAnswer() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User");
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setSurname("Test User");
+        userDTO.setPatronymic("Test User");
+        userDTO.setRole(Role.ПРЕПОДАВАТЕЛЬ);
+
+        userDTO = userService.createUser(userDTO);
+
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setId(0);
+        projectDTO.setFileLink("link");
+        projectDTO.setName("projectName");
+        projectDTO.setDescription("projectDescription");
+        projectDTO.setEndDate(LocalDateTime.now());
+        projectDTO.setStartDate(LocalDateTime.now());
+        projectDTO.setTeacherUserId(userDTO.getId());
+
+        projectDTO = projectService.createProject(projectDTO);
+
+        ProjectAnswerDTO projectAnswerDTO = new ProjectAnswerDTO();
+        projectAnswerDTO.setProjectId(projectDTO.getId());
+        projectAnswerDTO.setId(0);
+        projectAnswerDTO.setGrade(5);
+        projectAnswerDTO.setFileLink("link");
+        projectAnswerDTO.setSubmissionDate(LocalDateTime.now());
+        projectAnswerDTO.setStudentCommentary("good");
+        projectAnswerDTO.setTeacherCommentary("bad");
+        projectAnswerDTO = projectAnswerService.createProjectAnswer(projectAnswerDTO);
+
+        assertNotNull(projectAnswerDTO);
+    }
+
+    @Test
+    public void testUpdateProjectAnswer() {
+        ProjectAnswerDTO projectAnswerDTO = new ProjectAnswerDTO();
+        projectAnswerDTO.setId(1);
+        ProjectAnswerDTO result = projectAnswerService.updateProjectAnswer(projectAnswerDTO);
+        assertNull(result);
+    }
+
+    @Test
+    public void testDeleteProjectAnswer() {
+        Integer id = 1;
+        assertFalse(projectAnswerService.deleteProjectAnswer(id));
+    }
+}
---
+++ Backend/ProjectPalBack/src/test/java/cs/vsu/projectpalback/service/UserServiceTest.java
@@ -0,0 +1,197 @@
+package cs.vsu.projectpalback.service;
+
+import cs.vsu.projectpalback.dto.GroupDTO;
+import cs.vsu.projectpalback.dto.UserDTO;
+import cs.vsu.projectpalback.model.enumerate.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(true)
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private GroupService groupService;
+
+    @Rollback(true)
+    @BeforeEach
+    public void setup() {
+        GroupDTO group = new GroupDTO();
+        group.setGroupNumber(101);
+        group.setCourseNumber(1);
+        group = groupService.createGroup(group);
+
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User");
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setSurname("Test User");
+        userDTO.setPatronymic("Test User");
+        userDTO.setRole(Role.АДМИН);
+        userDTO.setGroupId(group.getId());
+
+        userService.createUser(userDTO);
+    }
+
+    @Test
+    public void testGetAllUsersWithoutPassword() {
+        assertNotNull(userService.getAllUsersWithoutPassword());
+    }
+
+    @Test
+    public void testGetAllUsersWithPassword() {
+        assertNotNull(userService.getAllUsersWithPassword());
+    }
+
+    @Test
+    public void testGetUserByIdWithoutPassword() {
+        assertNotNull(userService.getUserByIdWithoutPassword(1));
+    }
+
+    @Test
+    public void testGetUserByIdWithPassword() {
+        assertNotNull(userService.getUserByIdWithPassword(1));
+    }
+
+    @Test
+    public void testGetUsersByRole() {
+        assertNotNull(userService.getUsersByRole(Role.АДМИН));
+    }
+
+    @Test
+    public void testGetUsersByGroup() {
+        GroupDTO group = new GroupDTO();
+        group.setGroupNumber(101);
+        group.setCourseNumber(1);
+        group = groupService.createGroup(group);
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User");
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setSurname("Test User");
+        userDTO.setPatronymic("Test User");
+        userDTO.setRole(Role.ПРЕПОДАВАТЕЛЬ);
+        userDTO.setGroupId(group.getId());
+
+        userService.createUser(userDTO);
+        assertSame("Test User", userService.getUsersByGroup(group.getId()).get(0).getName());
+    }
+
+    @Test
+    public void testGetUserByLoginWithoutPassword() {
+        assertNotNull(userService.getUserByLoginWithoutPassword("test"));
+    }
+
+    @Test
+    public void testGetUserByLoginWithPassword() {
+        assertNotNull(userService.getUserByLoginWithPassword("test"));
+    }
+
+    @Rollback(true)
+    @Test
+    public void testCreateUser() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User");
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setSurname("Test User");
+        userDTO.setPatronymic("Test User");
+        userDTO.setRole(Role.АДМИН);
+        userDTO.setGroupId(3);
+        assertNotNull(userService.createUser(userDTO));
+    }
+
+    @Test
+    public void testDeleteUser() {
+        assertTrue(userService.deleteUser(1));
+    }
+
+    @Rollback(true)
+    @Test
+    public void testUpdateUser() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User 2");
+        userDTO.setSurname("Test User 2");
+        userDTO.setPatronymic("Test User 2");
+        userDTO.setRole(Role.АДМИН);
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setGroupId(2);
+
+
+        userDTO = userService.updateUser(userDTO);
+        assertSame(userDTO.getName(), userService.getUserByLoginWithoutPassword("test").getName());
+    }
+
+    @Rollback(true)
+    @Test
+    public void testUpdatePassword() {
+        assertTrue(userService.updatePassword(1, "newPassword"));
+    }
+
+    @Rollback(true)
+    @Test
+    public void testUpdateUserGroup() {
+        GroupDTO group = new GroupDTO();
+        group.setGroupNumber(101);
+        group.setCourseNumber(1);
+        group = groupService.createGroup(group);
+        assertTrue(userService.updateUserGroup(1, group.getId()));
+    }
+
+    @Rollback(true)
+    @Test
+    public void testUpdateUsersGroup() {
+        GroupDTO group = new GroupDTO();
+        group.setGroupNumber(101);
+        group.setCourseNumber(1);
+        group = groupService.createGroup(group);
+        assertTrue(userService.updateUsersGroup(Arrays.asList(1, 2), group.getId()));
+    }
+
+    @Rollback(true)
+    @Test
+    public void testCountUsersByGroup() {
+        GroupDTO group = new GroupDTO();
+        group.setGroupNumber(101);
+        group.setCourseNumber(1);
+        group = groupService.createGroup(group);
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test User");
+        userDTO.setLogin("test");
+        userDTO.setPassword("password");
+        userDTO.setSurname("Test User");
+        userDTO.setPatronymic("Test User");
+        userDTO.setRole(Role.АДМИН);
+        userDTO.setGroupId(group.getId());
+
+        userService.createUser(userDTO);
+
+        assertEquals(1, userService.countUsersByGroup(group.getId()));
+    }
+}
